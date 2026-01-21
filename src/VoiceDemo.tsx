f12b2dbd48d822f92136304938fa0edca6802f7b
Kombinatorin <instructorstulle@googlemail.com>
2026-01-21 03:56:01 +0100

    Add VoiceDemo component with basic layout

---
+++ src/VoiceDemo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const VoiceDemo: React.FC = () => {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: '#0b1020',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    }}>
+      <h1>InterviewOS</h1>
+      <p>Voice demo will live here.</p>
+    </div>
+  );
+};
+
+export default VoiceDemo;
